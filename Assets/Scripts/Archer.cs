d9a85f2ebefd7d62202d37f5b5505c73d3bd0c17
Vladyslav <piskovijvlad1@gmail.com>
2025-12-28 11:22:48 +0100

    add archer logic

---
+++ Assets/Scripts/Archer.cs
@@ -0,0 +1,9 @@
+﻿
+public class Archer : Warrior
+{
+    public Archer(int health, int damage, float speed, int fireRange, float fireRate, GridCell origin) 
+        : base(health, damage, speed, fireRange, fireRate, origin)
+    {
+    }
+
+}
